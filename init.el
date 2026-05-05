7fa5ef9eccd22170899fbd654c47249defeb527d
Gustavo L. <gustavolinssm@gmail.com>
2026-05-05 19:11:44 -0300

    chore(emacs): add initial init.el configuration

---
+++ init.el
@@ -0,0 +1,78 @@
+(require 'package)
+
+(setq package-archives
+      '(("gnu"    . "https://elpa.gnu.org/packages/")
+        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+        ("melpa"  . "https://melpa.org/packages/")))
+
+(package-initialize)
+
+(setq initial-scratch-message ";; Olá! Você sabia que duas vacas fazem muh muhhh? ;D\n\n")
+
+(use-package nyan-mode
+  :ensure t
+  :init (nyan-mode 1)
+  :config
+  (setq nyan-animate-nyancat t)
+  (setq nyan-wavy-trail t)
+  (nyan-start-animation))
+
+(use-package gruvbox-theme
+  :ensure t
+  :config (load-theme 'gruvbox-dark-medium t))
+
+(use-package lsp-mode
+  :ensure t
+  :hook (c-mode . lsp)
+  :commands lsp)
+
+(use-package corfu
+  :ensure t
+  :init (global-corfu-mode))
+
+(use-package cape
+  :ensure t)
+
+(use-package all-the-icons
+  :ensure t)
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+(use-package which-key
+  :ensure t
+  :init (which-key-mode))
+
+(use-package smartparens
+  :ensure t
+  :hook (prog-mode . smartparens-mode)
+  :config (require 'smartparens-config))
+
+(use-package treemacs
+  :ensure t
+  :bind ("C-c t" . treemacs))
+
+(use-package clang-format
+  :ensure t
+  :hook (c-mode . (lambda ()
+                    (add-hook 'before-save-hook #'clang-format-buffer nil t))))
+
+(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font-11")
+
+(setq completion-cycle-threshold 3)
+(setq tab-always-indent 'complete)
+
+(setq corfu-auto t)
+(setq corfu-cycle t)
+
+(setq inhibit-startup-screen t)
+(setq ring-bell-function 'ignore)
+(setq visible-bell nil)
+(global-display-line-numbers-mode 1)
+(column-number-mode 1)
+(set-fringe-mode 0)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(add-to-list 'default-frame-alist '(undecorated . t)) ;; Remove barra de título
